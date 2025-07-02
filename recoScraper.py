f0c0922feb2d9287767cfcd1517eaa9642bc1174
gussi55 <tiorg453@email.com>
2025-07-02 16:50:13 +0300

    0.8_1

--- recoScraper.py
+++ recoScraper.py
@@ -3,11 +3,19 @@ from src.DATAScrapper.recoParse import enterRegistrantSearch  ,getDataFromPAGE
 from src.BrowserInit import CreateBrowser , CreatePage
 import time
 from playwright.sync_api import sync_playwright
-from src.RandomCity.RandomCitySelect import selectRandomCity
+from src.RandomCity.RandomCitySelect import selectRandomCity,getListOfCities
+import random
+
+cities = getListOfCities("./data/ontarioCities.json")
 
 with sync_playwright() as p:
-    #driver = CreateBrowser()
-    page = CreatePage(p,PROFILE_PATH="./data/profile")
-    enterRegistrantSearch(selectRandomCity("./data/ontarioCities.json"),page=page)
-    getDataFromPAGE(page=page,url="./data/txt")
+    print(len(cities))
+    while len(cities) > 0 :
+        r=random.randint(0,len(cities))
+        #driver = CreateBrowser()
+        page = CreatePage(p,PROFILE_PATH="./data/profile")
+        enterRegistrantSearch(cities[r],page=page)
+        getDataFromPAGE(page=page,url="./data/txt")
+        cities.pop(r)
+        print(len(cities))
     
